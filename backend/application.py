48f4a8a4b06be8ce3ae88520b53b0a01c48b5edb
sc1f <tanjunyuan@hotmail.co.uk>
2016-02-12 00:37:56 -0600

    back to where we started but now with context

--- backend/application.py
+++ backend/application.py
@@ -15,22 +15,32 @@ app = Flask(__name__, static_folder=settings.static_files_location,
 	        template_folder=settings.template_folders_location)
 
 #datawork
+candidates = {}
+
 copy = copytext.Copy('dataset.xlsx')
 
-for row in copy['s1']:
-	candidateContext = {
-			'name': row['cand_name'].unescape()
-			'occupation': row['occu'].unescape()
-			'biography': row['bio'].unescape()
-
-		}
-
+for sheetName in copy.sheetNames():
+	for row in copy[sheetName]:
+		candidateContext = {
+				'name' : row['cand_name'].unescape(),
+				'occupation': row['occu'].unescape(),
+				'biography': row['bio'].unescape(),
+				'endorsements': row['endors'].unescape(),
+				'platform': row['plat'].unescape(),
+				'priority1': row['p1'].unescape(),
+				'priority2': row['p2'].unescape(),
+				'priority3': row['p3'].unescape(),
+				'twitter': row['twit'].unescape(),
+				'website': row['site'].unescape(),
+				'photocred': row['pcred'].unescape(),
+				'photo': row['purl'].unescape()
+			}
+	candidates = candidateContext
 #routing
 @app.route('/')
 def main_page():
-	context = {
-        'COPY': copytext.Copy('dataset.xlsx')
-    }
+	context = candidates
+	context['s1'] = copy['s1']
 	return render_template('index.html', **context)
 #debug
 if __name__ == '__main__': app.run(debug = True)
