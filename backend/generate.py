1c5311afbbc14007fc55bbfdda73c4cafec4c895
sc1f <tanjunyuan@hotmail.co.uk>
2016-02-17 21:08:30 -0600

    finalized build, some style changes

--- backend/generate.py
+++ backend/generate.py
@@ -4,18 +4,7 @@ from application import app
 
 import settings
 
-def generate():
-	app.config['FREEZER_DESTINATION'] = settings.web_app_location
-	app.config['FREEZER_BASE_URL'] = settings.external_url
+freezer = Freezer(app)
 
-	freezer = Freezer(app)
-
-	copy = copytext.Copy('dataset.xlsx')
-	@freezer.register_generator
-	def main_page():
-		for sheetName in copy.sheetNames():
-			if sheetName == 's1' : continue
-				
+if __name__ == '__main__': 
 	freezer.freeze()
-
-if __name__ == '__main__': generate()
--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,1 @@
-$(document).ready(function(){
-	$('hr.mini').hide();
-	var w = window.innerWidth;
-	if (w < 992){
-		$('hr.mini').show();
-	} else {
-		//do nothing
-	}
-})
+$(document).ready(function(){$('hr.mini').hide();var w = window.innerWidth;if (w < 992){$('hr.mini').show();} else {}});
